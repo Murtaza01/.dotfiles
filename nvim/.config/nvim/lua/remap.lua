450ddcd1a6e25149c6596fbfaa877cdf5bf3de39
Murtaza01 <merdada2000@hotmail.com>
2024-11-23 15:13:21 +0300

    added remap to nvim to delete extra spaces and capitlize line

--- nvim/.config/nvim/lua/remap.lua
+++ nvim/.config/nvim/lua/remap.lua
@@ -4,6 +4,10 @@ vim.keymap.set('n','<leader>f',"1z=")
 vim.keymap.set('n', '<leader>s', ":set invspell<CR>")
 -- writing mode
 vim.keymap.set('n','<leader>w',":ZenMode | :set nonu<CR>")
+-- Capitliaze Line
+vim.keymap.set('n','<leader>k',":s/\\<./\\U&/g<CR>")
+-- Delete Empty Spaces
+vim.keymap.set('n','<leader>l',":g/^$/,/./-1j<CR>")
 
 function AraType()
   vim.opt.keymap = "arabic_utf-8"
